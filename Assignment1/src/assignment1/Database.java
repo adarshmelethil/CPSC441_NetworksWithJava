a835f3e5a77862d1576c99a2817d92130f5f28d1
Adarsh Melethil <adarshmelethil@gmail.com>
2017-10-03 01:06:00 -0600

    First Commit

---
+++ Assignment1/src/assignment1/Database.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author cow
+ */
+public class Database {
+    Connection conn = null;
+    
+    public static Connection ConnectDB(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:CPSC441_Assignment1.sqlite");
+            
+            System.out.println("Connected to Database");
+            
+            return conn;
+        }catch(Exception ex){
+            Logger.getLogger(TCPClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public static void main(String args[]){
+        Connection conn = Database.ConnectDB();
+    }
+    
+}
